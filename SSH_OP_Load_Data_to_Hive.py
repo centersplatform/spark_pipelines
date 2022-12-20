04cd7d6b3d904001e92b3a97d9ae224da3cf76c0
hichamkabel <hichamkabel@gmail.com>
2022-12-20 17:35:30 +0100

    add new airflow dag that loads data to hive

---
+++ SSH_OP_Load_Data_to_Hive.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+
+default_args = {
+    'retries':2,
+    'owner': 'hive',
+}
+
+#cmd1='pwd'
+#cmd2='kubectl get pods -n airflow'
+access_spark_pod_cmd='kubectl exec -it spark-master-0 -n spark  -- '
+spark_submit_cmd="""spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App \
+tmp/NTTData-1.0-SNAPSHOT.jar LoadDataToDW --executor-memory 10g --driver-memory 10g
+"""
+
+with DAG(
+    dag_id='ssh_operator',
+    default_args=default_args,
+    start_date=datetime(2022, 12, 20),
+
+) as dag:
+    ssh_task = SSHOperator(
+		        ssh_conn_id= 'ssh_default', 
+		        task_id='ssh_submit_task', 
+                command= access_spark_pod_cmd+spark_submit_cmd,
+		        dag=dag
+    )
+
+ssh_task 
