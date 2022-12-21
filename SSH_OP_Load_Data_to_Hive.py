2a10b0331be6ab33238ba817bb3e5159e0a304f2
hichamkabel <hichamkabel@gmail.com>
2022-12-21 14:31:24 +0100

    load data into hive

--- SSH_OP_Load_Data_to_Hive.py
+++ SSH_OP_Load_Data_to_Hive.py
@@ -10,7 +10,7 @@ default_args = {
 
 #cmd1='pwd'
 #cmd2='kubectl get pods -n airflow'
-change_hadoop_user_cmd= "export HADOOP_USER_NAME=hive"
+#change_hadoop_user_cmd= "export HADOOP_USER_NAME=hive"
 access_spark_pod_cmd='kubectl exec -it spark-master-0 -n spark  -- '
 spark_submit_cmd="""spark-submit --master spark://spark-master-svc:7077 --class org.data_training.App \
 tmp/NTTData-1.0-SNAPSHOT.jar LoadDataToDW --executor-memory 10g --driver-memory 10g
@@ -19,13 +19,13 @@ tmp/NTTData-1.0-SNAPSHOT.jar LoadDataToDW --executor-memory 10g --driver-memory 
 with DAG(
     dag_id='SSH_Operator_Load_Data_to_HIVE',
     default_args=default_args,
-    start_date=datetime(2022, 12, 20),
+    start_date=datetime(2022, 12, 21),
 
 ) as dag:
     ssh_task = SSHOperator(
 		        ssh_conn_id= 'ssh_default', 
 		        task_id='ssh_submit_task', 
-                command= access_spark_pod_cmd+change_hadoop_user_cmd+" && "+spark_submit_cmd,
+                command= access_spark_pod_cmd+spark_submit_cmd,
 		        dag=dag
     )
 
